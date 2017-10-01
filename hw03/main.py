799cc5c4c9b668628212eb9280166b1db0086152
Song zengjia <inc_song@outlook.com>
2017-10-01 17:43:11 +0800

    Update main.py

--- hw03/main.py
+++ hw03/main.py
@@ -1,60 +1,52 @@
 from turtle import *
 import math as m
-
 def draw_flag(height):
+    #Draw a rectangle centered on the origin as flag
     up()
     goto(-0.75 * height, 0.5 * height)
     down()
-    color('red', 'red')
+    color('#f40002', '#f40002')
     setheading(0)
     begin_fill()
-    fd(1.5 * height)
-    right(90)
-    fd(height)
-    right(90)
-    fd(1.5 * height)
-    right(90)
-    fd(height)
+    for i in range(2):
+        fd(1.5 * height)
+        right(90)
+        fd(height)
+        right(90)
     end_fill()
-
-def draw_star(center, beginning):
+def draw_star(center, radius):
+    #Input the center and radius to draw a star
+    center_big_star = (-10 * x, 5 * x)
     up()
+    goto(center_big_star)
     goto(center)
-    goto(beginning)
-    down()
-    color('yellow', 'yellow')
-    radius = distance(center)
     setheading(0)
-    left(towards(center) + 18)
+    if center == center_big_star:
+        angle = 90
+    else:
+        angle = towards(center_big_star)
+    #The 'angle' means the orientation of star
+    #It's according to the relationship with the big star
+    left(angle)       
+    fd(radius)
+    right(162)
+    down()
+    beginning = pos()
+    color('#faf408', '#faf408')    
     begin_fill()
     while True:
         fd(2 * radius * m.cos(m.pi / 10))
         right(144)
         if distance(beginning) < 1:
             break
-    end_fill()
-    
-def node_little_star(center_little_star, radius):
-    center_big_star = (-10 * x, 5 * x)
-    up()
-    goto(center_big_star)
-    goto(center_little_star)
-    setheading(0)
-    left(towards(center_big_star))
-    fd(radius)
-    node = pos()
-    return node
-
+    end_fill()   
+height = 460 #Set the height of the flag
+x = height / 20 #The x represents a unit length
 speed(7)
-height = 640
 draw_flag(height)
-x = height / 20
-
-draw_star((-10 * x, 5 * x), (-10 * x, 8 * x))
-
-draw_star((-5 * x, 8 * x), node_little_star((-5 * x, 8 * x), x))
-draw_star((-3 * x, 6 * x), node_little_star((-3 * x, 6 * x), x))
-draw_star((-3 * x, 3 * x), node_little_star((-3 * x, 3 * x), x))
-draw_star((-5 * x, 1 * x), node_little_star((-5 * x, 1 * x), x))
-
+draw_star((-10 * x, 5 * x), 3 * x)
+draw_star((-5 * x, 8 * x), x)
+draw_star((-3 * x, 6 * x), x)
+draw_star((-3 * x, 3 * x), x)
+draw_star((-5 * x, 1 * x), x)
 hideturtle()
